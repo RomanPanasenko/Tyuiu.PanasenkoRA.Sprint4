6eef1ec0c94be79ac0ffe9eedbca8cada5d1ef42
Tyuiu.PanasenkoRA@outlook.com <Tyuiu.PanasenkoRA@outlook.com>
2023-11-13 20:33:48 +0500

    Task6

---
+++ Tyuiu.PanasenkoRA.Sprint4.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanasenkoRA.Sprint4.Task6.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint4.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var tex = new string[] { "Компьютер", "Телефон", "Планшет", "Принтер", "Сканер", "Монитор", "Клавиатура" };
+            var res = ds.Calculate()
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint4.Task6.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PanasenkoRA.Sprint4.Task6.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint4.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
