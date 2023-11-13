8865a473703cf99e0b18e4cb438060585a381029
Tyuiu.PanasenkoRA@outlook.com <Tyuiu.PanasenkoRA@outlook.com>
2023-11-13 12:04:25 +0500

    Task5

---
+++ Tyuiu.PanasenkoRA.Sprint4.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.PanasenkoRA.Sprint4.Task5.V0.Lib
+{
+    public class DataService : ISprint4Task5V10
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sum = 0;
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint4.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanasenkoRA.Sprint4.Task5.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint4.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            
+            int[,] mtrx = new int[3, 3] { { -3, 5, 1 },
+                                          { -5, 6, -3 },
+                                          { 7, 1, -4 } };
+            int res = ds.Calculate(mtrx);
+            int wait = 20;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
